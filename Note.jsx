545774d623c7acead08a40766023e0a1d35fde21
TheNamanSethi <85399626+TheNamanSethi@users.noreply.github.com>
2021-06-05 18:47:13 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header(){
+    return (
+        <div className="note">
+            <h1>Javascript and React.js</h1>
+            <p>
+                This was an marvalous bootcamp taken by Shaurya Sir.
+                THANKYOU SO MUCH SIR.
+                We covered everything from Scratch including JavaScript,React.js, HTML.
+                SHAPE AI is working amazingly, they are helping students in such a great way. I SALUTE,their work is builing a great future of our nation.
+                
+            </p>
+        </div>
+    );
+}
+
+export default Note;
